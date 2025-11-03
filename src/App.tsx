dedcfd6d4ca690ad99f0e6ee8f6bac1bcce12c57
Anton <komogorcevanton10@gmail.com>
2025-11-03 13:26:02 +0800

    feat: maked header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import logo from './assets/favicon/icon.svg'
+
+import './styles/header.css'
+
 const App = () => {
   return (
     <> 
-    
+      <header>
+        <a className='logo' href="/">
+          <img src={logo} alt="" />
+          <h1>Ink. House</h1>
+        </a>
+
+        <nav>
+          <a className='nav-buttons' href="/">Репродукции</a>
+          <a className='nav-buttons' href="/">Новинки</a>
+          <a className='nav-buttons' href="/">О нас</a>
+          <a className='basket' href="/"></a>
+        </nav>
+      </header>
     </>
   )
 }
