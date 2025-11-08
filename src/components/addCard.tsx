3c38d25de23400a149226d8701ff7da3128e2111
Anton <komogorcevanton10@gmail.com>
2025-11-08 17:47:25 +0800

    feat: add cards in the catalog body

---
+++ src/components/addCard.tsx
@@ -0,0 +1,21 @@
+import '../styles/addcard.css'
+
+const AddCard = (props: any) => {
+    return (
+        <article className='card'>
+            <section className='card-header'>
+                <img className='card-image' src={props.src} alt="" />
+                <p className='card-creator'>{props.creator}</p>
+                <h3 className='card-name'>{props.name}</h3>
+                <p className='card-feature'>{props.feature}</p>
+            </section>
+            
+            <section className='card-bottom'>
+                <p className='card-price'>{props.price}</p>
+                <button className='add-to-basket'>В корзину</button>
+            </section>
+        </article>
+    )
+}
+
+export default AddCard
