616c4a47e3fc31e765e4696b105ba713d1e02b24
Anton <komogorcevanton10@gmail.com>
2025-11-12 19:57:29 +0800

    feat: added footer

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,17 @@ import FranceCardSection from './components/franceCardsSection'
 import GermanyCardSection from './components/germanyCardsSection'
 import EnglandCardSection from './components/englandCardsSection'
 
+import facebook from './assets/sections/footer/facebook.svg'
+import youtube from './assets/sections/footer/youtube.svg'
+import instagram from './assets/sections/footer/instagram.svg'
+
 import './styles/header.css'
 import './styles/reset.css'
 import './styles/hero.css'
 import './styles/catalog.css'
 import './styles/promo.css'
 import './styles/our-team.css'
+import './styles/footer.css'
 
 const App = () => {
   const [stateButtonSwitchCards, setStateButtonSwitchCard] = useState('france')
@@ -110,6 +115,60 @@ const App = () => {
           </section>
         </section>
       </main>
+
+      <footer>
+        <section className="footer-left">
+          <div className="logo">
+            <img src={logo} alt="" />
+            <h2 className='icon'>Ink. House</h2>
+          </div>
+
+          <section className='footer-left-bottom'>
+            <p style={{ color: '#2C2D35' }}>+7 (999) 543-54-54</p>
+            <p>Мастерская</p>
+          </section>
+        </section>
+
+        <section className="footer-center">
+          <section className='footer-column-one footer-center-columns'>
+            <h2>Репродукции</h2>
+            <section className='columns-body'>
+              <p>Франция</p>
+              <p>Германия</p>
+              <p>Англия</p>
+            </section>
+          </section>
+
+          <section className="footer-column-two footer-center-columns">
+            <h2>Новинки</h2>
+            <section className='columns-body'>
+              <p>2021</p>
+              <p>2020</p>
+            </section>
+          </section>
+
+          <section className="footer-column-three footer-center-columns">
+            <h2>О нас</h2>
+            <section className='columns-body'>
+              <p>Художники</p>
+              <p>Менеджеры</p>
+            </section>
+          </section>
+        </section>
+
+        <section className="footer-right">
+          <section className="messanger-logos">
+            <img src={facebook} alt="" />
+            <img src={instagram} alt="" />
+            <img src={youtube} alt="" />
+          </section>
+
+          <section className='rights'>
+            <p>Ink. House ®</p>
+            <p>All rights reserved</p>
+          </section>
+        </section>
+      </footer>
     </>
   )
 }
